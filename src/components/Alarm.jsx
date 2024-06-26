c1cf6bc47612a594148bd2a0b11f5fc3692f462f
Sinan Muhammed <sinan9539391118@gmail.com>
2024-06-26 20:34:09 +0530

    Alarm cars passing props

--- src/components/Alarm.jsx
+++ src/components/Alarm.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Alarm = () => {
   return (
     <div className='flex-1 m-4 '>
-        <Card/>
+        <Card 
+        time={"5:00"}
+        am={true}
+        date={"Mon, Tue"}
+        enabled={true}
+        />
     </div>
   )
 }
 
 export default Alarm
 
-const Card = () => {
+const Card = ({time, am, date, enabled}) => {
+    const [user,setUser] = useState(enabled)
     return (
     <div className='p-4 border border-red-600 rounded-xl flex justify-between'>
         <div>
-            <div>next in 5h</div>
+            {user && <div>next in 5h</div>} 
             <div className=' flex flex-auto gap-1 align-bottom'>
-                <div className=' text-6xl'>5:00</div>
-                <div className=' pb-1 flex items-end '>am</div>
+                <div className=' text-6xl'>{time}</div>
+                <div className=' pb-1 flex items-end '>{am ? "am" : "pm"}</div>
             </div>
-            <div>Mon,Tue</div>
+            <div>{user ? date : "Not Scheduled"}</div>
         </div>
         <div className='flex flex-col'>
             <div className=' flex-1'>
@@ -29,7 +35,9 @@ const Card = () => {
             </div>
             <div>
                 <label class="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" class="sr-only peer" checked></input>
+                    <input type="checkbox" onChange={
+                        () => { setUser(!user)}
+                    } value="" class="sr-only peer" checked={user} ></input>
                     <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                 </label>
             </div>
