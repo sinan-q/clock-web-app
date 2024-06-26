6c8e6549fd6dbde897694d36059c13c5333256ca
Sinan Muhammed <sinan9539391118@gmail.com>
2024-06-26 20:34:54 +0530

    BottomBar passing props

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -3,21 +3,37 @@ import React from 'react'
 const BottomBar = () => {
   return (
     <div className='flex gap-2 justify-center px-5 w-dvw'>
-            <BottomBarElement/>
-            <BottomBarElement/>
-            <BottomBarElement/>
-            <BottomBarElement/>
-            <BottomBarElement/>
+            <BottomBarElement
+              icon={"alarm"}
+              label={"Alarm"}
+            />
+            <BottomBarElement
+              icon={"schedule"}
+              label={"Clock"}
+            />
+            <BottomBarElement
+              icon={"hourglass_bottom"}
+              label={"Timer"}
+            />
+            <BottomBarElement
+              icon={"timer"}
+              label={"StopWatch"}
+            />
+            <BottomBarElement
+              icon={"bedtime"}
+              label={"BedTime"}
+            />
+            
 
     </div>
   )
 }
 
-const BottomBarElement = () => {
+const BottomBarElement = ({icon, label}) => {
     return (
-                <a href='#' class=" "    >
-                    <div className="material-icons content-center px-2 pt-2 w-fit">alarm</div>
-                    <p className='w-fit'>Alarm</p>
+                <a href='#' class="flex items-center flex-col"    >
+                    <div className="material-icons px-2 pt-2 w-fit">{icon}</div>
+                    <p className=''>{label}</p>
                 </a>  
     
     )
