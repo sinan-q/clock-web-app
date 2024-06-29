41259d1a84b5d30c05ebad952c65338560e71e01
Sinan Muhammed <sinan9539391118@gmail.com>
2024-06-29 14:02:09 +0530

    added onClick function for FAB

--- src/components/FAB.jsx
+++ src/components/FAB.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FAB = ({icon}) => {
+const FAB = ({icon , onClic = ()=> {}}) => {
   return (
     <div class="fixed bottom-16 right-6">
-            <button class="material-icons bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-4 rounded-2xl shadow-lg">
+            <button onClick={onClic} className="material-icons bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-4 rounded-2xl shadow-lg">
                 {icon}
             </button>
     </div>
