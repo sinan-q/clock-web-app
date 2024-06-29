88eb4bb79d1314d86c713954c37ac1edab569cd5
Sinan Muhammed <sinan9539391118@gmail.com>
2024-06-29 14:02:38 +0530

    Added functionality Stopwatch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import FAB from './FAB'
 
 const Stopwatch = () => { 
-  return (
+    const [timer, setTimer] = useState(0)
+    const [startTimer, setStartTimer] = useState(false)
+    useEffect(()=> {
+        let interval = null        
+        if (startTimer) {
+            interval = setInterval(() => {
+                setTimer(prevTime => prevTime + 17)
+            }, 17);
+        } 
 
-    <>
-    <div className='flex-1 m-4 flex flex-col items-center justify-center '>
-        <div className="border-8 border-gray-600 w-64 h-64 content-center rounded-full  ">
-            <div className=" text-center text-6xl">00</div>
-            <div className=" text-center leading-3 pl-8 text-3xl  ">00</div>
+        return () => clearInterval(interval)
+    }, [startTimer])
+
+    return (
+
+        <>
+        <div className='flex-1 m-4 flex flex-col items-center justify-center '>
+            <div className="border-8 border-gray-600 w-64 h-64 content-center rounded-full  ">
+                <div className=" text-center text-6xl">{timer/1000|0}</div>
+                <div className=" text-center leading-3 pl-8 text-3xl  ">{timer%1000}</div>
+            </div>
         </div>
-    </div>
-    <FAB 
-    icon={"play_arrow"}
-    />
-    </>
-  )
-}
+        <FAB 
+        icon={"play_arrow"}
+        onClic = {() => {
+            setStartTimer((prevStart) => !prevStart)}}
+        />
+        </>
+    )
+    }
 
 export default Stopwatch
