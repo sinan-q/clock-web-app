53a47660a4e89a5dd408d7806d47e56e74a2167b
Sinan Muhammed <sinan9539391118@gmail.com>
2024-06-29 12:14:19 +0530

    Added Dynamc top bar title

--- src/components/TopAppBar.jsx
+++ src/components/TopAppBar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const titles = {
+  "/alarm": "Alarm",
+  "/clock": "Clock",
+  "/timer": "Timer",
+  "/stopwatch": "Stopwatch",
+  "/bedtime" :"Bedtime"
+};
 
 const TopAppBar = () => {
+  const location = useLocation();
+  const [title, setTitle] = useState(titles["/"]);
+
+  useEffect(() => {
+    setTitle(titles[location.pathname]);
+  }, [location.pathname]);
+
   return (
     <div className='flex justify-between p-4'>
-        <div className=' text-xl'>Top App Bar</div>
+        <div className=' text-xl'>{title}</div>
         <div className='material-icons'>more_vert</div>
     </div>
   )
