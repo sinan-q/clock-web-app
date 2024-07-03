5503a181a1a1a13081d616afe194723f882b9d4a
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-03 19:21:33 +0530

    Fixed FAB Hide scrollbar

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,23 +3,26 @@ import FAB from './FAB'
 
 const Clock = () => {
   return (
-    <div className='flex-1 m-4 pt-8 w-sreen'>
-            <div className='flex items-center flex-col'>
-                <div className='flex items-end'>
-                    <div className=' text-5xl'>12:00</div>
-                    <div className=' align-text-bottom'>am</div>
-                </div>
-                <div className='mt-3'>
-                    Tue, 25 Jun
-                </div>
+    <>
+    <div className='flex-1 m-4 pt-8'>
+        <div className='flex items-center flex-col'>
+            <div className='flex items-end'>
+                <div className=' text-5xl'>12:00</div>
+                <div className=' align-text-bottom'>am</div>
             </div>
-            <div>
-                
+            <div className='mt-3'>
+                Tue, 25 Jun
             </div>
-            <FAB 
-                icon={"add"}
-            />
+        </div>
+        <div>
+            
+        </div>
+            
     </div>
+    <FAB 
+        icon={"add"}
+    />
+    </>
   )
 }
 
--- src/components/FAB.jsx
+++ src/components/FAB.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FAB = ({icon , onClic = ()=> {}}) => {
   return (
-    <div class="fixed bottom-16 right-6">
+    <div class="flex fixed fab bottom-28 justify-end">
             <button onClick={onClic} className="material-icons bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-4 rounded-2xl shadow-lg">
                 {icon}
             </button>
