4999bb64fd2f68c00c9d1753b8d185163a50d5f1
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-03 19:37:13 +0530

    StatusBar size changes

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const StatusBar = () => {
   return (
     <div className="flex items-center justify-between px-4 bg-gray-200">
-      <span className="text-lg">9:30</span>
-      <div className="flex space-x-2">
-        <i className="material-icons fas fa-heart ">signal_cellular_4_bar</i>
-        <i className="material-icons">battery_3_bar</i>
+      <span className="text-sm">9:30</span>
+      <div className="flex space-x-1">
+        <i className="material-icons text-base ">signal_cellular_4_bar</i>
+        <i className="material-icons text-base">battery_3_bar</i>
       </div>
     </div>
   )
