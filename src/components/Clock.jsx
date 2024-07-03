e8b60e8fa940e61a4962ff77e12a2481037f7d6f
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-03 21:22:29 +0530

    Added functional Clock with moment.js

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import FAB from './FAB'
+import moment from 'moment/moment'
 
 const Clock = () => {
+    const [time, setTime] = useState("loading")
+    const [timeam, setTimeam] = useState("")
+    const [date, setDate] = useState("loading")
+
+
+    useEffect(() => {
+
+        setInterval(() => {
+    
+            const dateObject = moment()  
+            setTimeam(dateObject.format('a'))
+            setTime( dateObject.format(' h:mm'))
+            setDate(dateObject.format('ddd, d MMM'))
+        }, 1000)
+    
+      }, [])
   return (
     <>
     <div className='flex-1 m-4 pt-8'>
         <div className='flex items-center flex-col'>
             <div className='flex items-end'>
-                <div className=' text-5xl'>12:00</div>
-                <div className=' align-text-bottom'>am</div>
+                <div className=' text-5xl'>{time}</div>
+                <div className='ml-2 align-text-bottom'>{timeam}</div>
             </div>
-            <div className='mt-3'>
-                Tue, 25 Jun
+            <div className='text-xs'>
+                {date}
             </div>
         </div>
         <div>
