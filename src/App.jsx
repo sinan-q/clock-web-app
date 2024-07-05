f1d5c8bef101cccf1164ebe622d0046397d61041
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-05 11:48:56 +0530

    Background blur shapes from wweb.dev/resources/animated-css-background-generator

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import PageRoutes from './components/PageRoutes'
 
 const App = () => {
   return (  
-    <div className="w-dvw h-dvh flex overflow-hidden z-10 justify-center">
+    <div className=" root w-dvw h-dvh flex overflow-hidden z-10 justify-center">
+        <span></span>        <span></span>        <span></span>        <span></span> <span></span>
     <div class=" bg-white box-content border-4 w-80 relative my-10 border-cyan-900 flex flex-col">
       <StatusBar></StatusBar>
       <TopAppBar/>
