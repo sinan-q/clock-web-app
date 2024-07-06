aacceb9ebf2809f78f602bcef3a523cd2207ca20
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-06 11:21:56 +0530

    Timerbutton init from list

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -7,7 +7,7 @@ const Timer = () => {
         setTimer((prev) => {
             let t = prev.slice()
             switch (text) {
-                case "del":
+                case "<":
                     t.pop()
                     t.unshift(0)
                     break;
@@ -37,65 +37,10 @@ const Timer = () => {
                 <div className="text-sm pb-1">s</div>
             </div>
             <div className=" grid grid-cols-3 place-items-center mt-6 mx-6">
-                <TimerButton 
-                    text= {1}
-                    onClick = {()=>{setTime(1)}}
-                />
-                <TimerButton 
-                    text= {2}
-                    onClick = {()=>{setTime(2)}}
-
-                />
-                <TimerButton 
-                    text= {3}
-                    onClick = {()=>{setTime(3)}}
-
-                />
-                <TimerButton 
-                    text= {4}
-                    onClick = {()=>{setTime(4)}}
-
-                />
-                <TimerButton 
-                    text= {5}
-                    onClick = {()=>{setTime(5)}}
-
-                />
-                <TimerButton 
-                    text= {6}
-                    onClick = {()=>{setTime(6)}}
-
-                />
-                <TimerButton 
-                    text= {7}
-                    onClick = {()=>{setTime(7)}}
-
-                />
-                <TimerButton 
-                    text= {8}
-                    onClick = {()=>{setTime(8)}}
-
-                />
-                <TimerButton 
-                    text= {9}
-                    onClick = {()=>{setTime(9)}}
-
-                />
-                <TimerButton 
-                    text= {"00"}
-                    onClick = {()=>{setTime("00")}}
-
-                />
-                <TimerButton 
-                    text= {0}
-                    onClick = {()=>{setTime(0)}}
-
-                />
-                <TimerButton 
-                    text= {"<"}
-                    onClick = {()=>{setTime("del")}}
-
-                />
+                {[1,2,3,4,5,6,7,8,9,"00",0,"<"].map((item) => <TimerButton 
+                    text= {item}
+                    onClick = {()=>{setTime(item)}}
+                />)}
             </div>
         </div>
         {timer.some((num) => num!=0) && <FAB
