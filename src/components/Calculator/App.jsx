5ce37bfb1e6269b8ca1e0a1f6d900f54651093ba
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-16 14:29:37 +0530

    Added NavBar in Calculator

--- src/components/Calculator/App.jsx
+++ src/components/Calculator/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Calculator from './Calculator'
+import { NavBar } from '../BottomBar'
 
 const CalculatorApp = () => {
   return (
+    <>
     <Calculator/>
+    <NavBar />
+    </>
+    
   )
 }
 
